63dad6e53bb455370b96ff2db09ece85abbda51d
matteus-henryk <matteus.henryk2@gmail.com>
2020-10-15 23:01:28 -0300

    FEATURE: aula 4 nlw, features de renderizar as infromações dos orfanatos pelo backend

--- public/scripts/page-orfanages.js
+++ public/scripts/page-orfanages.js
@@ -7,22 +7,40 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 // CRIAR ICONE
 const icon = L.icon({
-  iconUrl: "./public/images/map-marker.svg",
+  iconUrl: "/images/map-marker.svg",
   iconSize: [58, 68],
   iconAnchor: [29, 68],
   popupAnchor: [170, 2]
 })
 
-//CRIAR POPUP OVERLAY
-const popup =L.popup({
-  closeButton: false,
-  className: 'map-popup',
-  minWidth: 240,
-  minHeight: 240,
+function addMarker({ id, name, lat, lng }) {
+  console.log(id);
 
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
+  //CRIAR POPUP OVERLAY
+  const popup =L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240,
+  
+  }).setContent(`${name}<a href="/orphanage?id=${id}"><img src="/images/arrow-white.svg"></a>`)
+  
+  // CRIAR E ADD MATKER
+  L.marker([lat,lng], { icon })
+      .addTo(map)
+      .bindPopup(popup);
 
-// CRIAR E ADD MATKER
-L.marker([-23.3281339,-51.1693468], { icon })
-    .addTo(map)
-    .bindPopup(popup);
+}
+
+const orphanagesSpan = document.querySelectorAll('.orphanages span');
+
+orphanagesSpan.forEach(span => {
+  const orphanage = {
+    id: span.dataset.id,
+    name: span.dataset.name,
+    lat: span.dataset.lat,
+    lng: span.dataset.lng
+  }
+
+  addMarker(orphanage);
+})
---
+++ src/database/fakedata.js
@@ -0,0 +1,50 @@
+module.exports = [
+  {
+    id: 1,
+
+    name: "lar das meninos",
+
+    description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+
+    images: [
+      "https://images.unsplash.com/photo-1574647258357-e19ec1bcc09c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+      "https://images.unsplash.com/photo-1556258707-995cd393dd8b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+    ],
+
+    lat: "-23.3281311",
+
+    lng: "-51.1693459",
+
+    instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+
+    opening_hours: "Horário de visitas Das 18h até 8h",
+
+    open_on_weekends: "1"
+
+  },
+  {
+    id: 2,
+
+    name: "lar das meninas",
+
+    description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+
+    images: [
+      "https://images.unsplash.com/photo-1556258707-995cd393dd8b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+      "https://images.unsplash.com/photo-1574647258357-e19ec1bcc09c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+    ],
+
+    lat: "-23.618237",
+
+    lng: "-46.635197",
+
+    instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+
+    opening_hours: "Horário de visitas Das 18h até 8h",
+
+    open_on_weekends: "0"
+
+  }
+]
